b786e177ce18652edadbde6fc52e57ad2620965b
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2021-10-17 21:23:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function imc() {
         let classificacao = '';
         let ideal = '';
 
+        if (sexo == "masculino") {
+            if (altura < '1.57') {
+                ideal == 'Seu peso ideal e menor que 52KG';
+            } else if (altura > '1.57' && altura < '1.70') {
+                ideal == 'Seu peso ideal esta entre 52-69KG';
+            } else if (altura > '1.70' && altura < '1.80') {
+                ideal == 'Seu peso ideal esta entre 70-85KG';
+            } else if (altura > '1.80' && altura < '1.93') {
+                ideal == 'Seu peso ideal esta entre 86-100KG';
+            }
+        } else if (sexo == "feminino") {
+            if (altura < '1.50') {
+                ideal == 'Seu peso ideal e menor que 45KG';
+            } else if (altura > '1.50' && altura < '1.60') {
+                ideal == 'Seu peso ideal esta entre 45-53KG';
+            } else if (altura > '1.60' && altura < '1.75') {
+                ideal == 'Seu peso ideal esta entre 54-69KG';
+            } else if (altura > '1.75' && altura < '1.85') {
+                ideal == 'Seu peso ideal esta entre 70-82KG';
+            }
+        }
+
         if (valorIMC < 16) {
             classificacao = 'abaixo do peso (Grau 1).';
         } else if (valorIMC < 17) {
@@ -31,28 +53,6 @@ function imc() {
             classificacao = 'com obesidade grau II.';
         } else {
             classificacao = 'com obesidade grau III.';
-        }
-
-        if (sexo == "Masculino") {
-            if (altura < 1.57) {
-                ideal == 'Seu peso ideal e menor que 52KG';
-            } else if (altura > 1.57 && altura < 1.70) {
-                ideal == 'Seu peso ideal esta entre 52-69KG';
-            } else if (altura > 1.70 && altura < 1.80) {
-                ideal == 'Seu peso ideal esta entre 70-85KG';
-            } else if (altura > 1.80 && altura < 1.93) {
-                ideal == 'Seu peso ideal esta entre 86-100KG';
-            }
-        } else if (sexo == "Feminino") {
-            if (altura < 1.50) {
-                ideal == 'Seu peso ideal e menor que 45KG';
-            } else if (altura > 1.50 && altura < 1.60) {
-                ideal == 'Seu peso ideal esta entre 45-53KG';
-            } else if (altura > 1.60 && altura < 1.75) {
-                ideal == 'Seu peso ideal esta entre 54-69KG';
-            } else if (altura > 1.75 && altura < 1.85) {
-                ideal == 'Seu peso ideal esta entre 70-82KG';
-            }
         }
 
         resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}, com seu peso ideal igual a ${ideal}`;
